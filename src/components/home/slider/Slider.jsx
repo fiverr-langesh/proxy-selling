dd389d7bc1afd9d9c9a60b5b61e6db334b121c8c
langesh <dev.langesh@gmail.com>
2023-11-27 13:35:41 +0530

    completed slider

--- src/components/home/slider/Slider.jsx
+++ src/components/home/slider/Slider.jsx
@@ -28,6 +28,16 @@ const CustomSlider = () => {
     }
   };
 
+  const handleClick = (event) => {
+    const sliderRect = sliderRef.current.getBoundingClientRect();
+    const offsetX = event.clientX - sliderRect.left;
+    const percent = (offsetX / sliderRect.width) * 100;
+
+    // Ensure the value stays within the valid range (0 to 100)
+    const newValue = Math.min(100, Math.max(0, percent));
+    setSliderValue(newValue);
+  };
+
   return (
     <div
       className="custom-slider"
@@ -35,22 +45,33 @@ const CustomSlider = () => {
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseMove={handleMouseMove}
+      onClick={handleClick}
     >
+      <div className="slider-circle start"></div>
       <div className="slider-track" style={{ width: `${sliderValue}%` }}></div>
       <div
         className="slider-highlight"
         style={{ width: `${sliderValue}%` }}
       ></div>
       <div
+        onmousedown={() => false}
         className="slider-thumb"
-        style={{ left: `calc(${sliderValue}% - 25px)` }}
+        style={{ left: `calc(${sliderValue}%)` }}
       >
         {Math.round(sliderValue)}
       </div>
-      <div
-        className="slider-background"
-        style={{ left: `${sliderValue}%` }}
-      ></div>
+      <div className="slider-background"></div>
+      <div className="slider-circle end"></div>
+      <div className="scale">
+        {[...Array(20).keys()].map((index) => (
+          <div
+            key={index}
+            className={`scale-line ${
+              index / 20 <= sliderValue / 100 ? "highlighted" : ""
+            }`}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
