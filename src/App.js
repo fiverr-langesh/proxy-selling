7e3496764b9a9fa69a3b4959aebd57b4a5187fc4
Nithishpandian <nithishpandiyan75@gmail.com>
2023-11-27 10:03:51 +0530

    created auth files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/common/Navbar";
 import Banner from "./components/home/Banner";
+import LoginComponent from "./pages/auth/Login";
 
 function App() {
   return (
     <main className="w-screen h-screen overflow-x-hidden overflow-y-auto bg-primary px-[30px] sm:px-[100px] md:px-[130px] pt-[60px] font-urbanist">
-      <Navbar />
-      <Banner />
+      {/* <Navbar />
+      <Banner /> */}
+      <LoginComponent />
     </main>
   );
 }
---
+++ src/components/auth/LoginComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginComponent() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default LoginComponent
