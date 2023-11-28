4bc5ac07c4e4eb2061ed370cc3c74505ff2c0667
Nithishpandian <nithishpandiyan75@gmail.com>
2023-11-28 17:06:01 +0530

    position correction

--- src/components/proxy/Proxy.jsx
+++ src/components/proxy/Proxy.jsx
@@ -7,7 +7,7 @@ function Proxy() {
     <>
       <section className="flex items-center justify-center flex-wrap px-[10px] sm:px-[20px] md:px-[10px] lg:px-[20px] xl:px-[50px] pt-14 md:pt-8 lg:pt-14 font-urbanist">
         <div className="md:w-1/2 flex items-center justify-center space-y-5 text-darkSecondary">
-          <div className=" max-[400px]:w-[350px] max-[500px]:w-[400px] sm:w-[500px] bg-secondaryLogin rounded-lg py-10 px-5 sm:px-10 max-[400px]:text-xl min-[400px]:text-2xl drop-shadow-[0_0px_70px_rgba(255,255,255,0.75)]">
+          <div className=" max-[400px]:w-[350px] sm:w-[500px] bg-secondaryLogin rounded-lg py-10 px-5 sm:px-10 max-[400px]:text-lg min-[400px]:text-2xl drop-shadow-[0_0px_70px_rgba(255,255,255,0.75)]">
             <p className=" font-medium">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
