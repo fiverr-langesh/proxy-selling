86c1448dfcaac8d1edc72fde93d3fa2f8290c98d
langesh <dev.langesh@gmail.com>
2023-11-29 21:40:49 +0530

    footer bug fix

--- src/components/proxy/Proxy.jsx
+++ src/components/proxy/Proxy.jsx
@@ -4,7 +4,7 @@ import Footer from "../common/Footer";
 
 function Proxy() {
   return (
-    <main className="overflow-x-hidden overflow-y-auto xl:pt-16 px-[15px] sm:px-[25px] md:px-[100px] lg:px-[50px] font-urbanist">
+    <>
       <section className="flex items-center justify-center flex-wrap mt-12 xl:mt-0">
         <div className="md:w-[668px] flex items-center justify-center space-y-5 text-darkSecondary">
           <div className=" max-[400px]:w-[350px] sm:w-[500px] bg-secondaryLogin rounded-lg py-10 px-5 sm:px-10 max-[400px]:text-lg min-[400px]:text-2xl drop-shadow-[0_0px_70px_rgba(255,255,255,0.75)]">
@@ -30,7 +30,7 @@ function Proxy() {
         </div>
       </section>
       <Footer />
-    </main>
+    </>
   );
 }
 
