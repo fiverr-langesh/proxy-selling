7115ec696238a9ac1a9f4ff339c5d08198e3a3fa
langesh <dev.langesh@gmail.com>
2023-11-29 21:35:41 +0530

    logout

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigateButton from "./buttons/NavigateButton";
+import Button from "./buttons/Button";
 
 export default function Navbar() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) setToken(token);
+  }, []);
+
+  function logout() {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  }
+
   return (
     <div className="flex items-center justify-between bg-primary pt-[30px] px-[15px] sm:px-[25px] md:px-[100px] lg:px-[50px ">
       <img width={110} height={42} src="images/logo.png" alt="" />
 
       <div className="flex items-center space-x-5">
-        <NavigateButton
-          text="Login"
-          href="/login"
-          customStyle={" w-[90px] sm:w-[120px]"}
-        />
-        <NavigateButton
-          text="Signup"
-          href="/signup"
-          customStyle={" w-[90px] sm:w-[120px]"}
-        />
+        {
+          token ? (
+            <>
+              <Button
+                customStyle="tracking-widest"
+                text="Logout"
+                href="/dashboard"
+                onClick={logout}
+              />
+             
+            </>
+          ) : (
+            <>
+              <NavigateButton
+                customStyle="tracking-widest"
+                text="Login"
+                href="/login"
+              />
+              <NavigateButton
+                customStyle="tracking-widest"
+                text="Register"
+                href="/register"
+              />
+            </>
+          )
+       }
       </div>
     </div>
   );
--- src/components/common/buttons/Button.jsx
+++ src/components/common/buttons/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function NavigateButton({ text, type,customStyle }) {
+export default function NavigateButton({ text, type,customStyle,onClick }) {
   return (
     <button
       type={type}
+      onClick={onClick}
       className={`bg-gradient-to-r from-gl to-gr w-[120px] h-[45px] rounded-[100px] text-[16px] text-white font-semibold inline-block ${customStyle}`}
     >
       {text}
