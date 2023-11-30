e6451520866db1d5ee0ab41b156446270645022f
langesh <langesh@gmail.com>
2023-11-30 12:24:10 +0530

    client modifications  ( number )

--- src/components/proxy-location/ProxyCountry.jsx
+++ src/components/proxy-location/ProxyCountry.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Button from "../common/buttons/Button";
 
-function ProxyCountry({ image, country}) {
+function ProxyCountry({ image, country, number }) {
   return (
     <div className="m-4 flex flex-col justify-center items-center rounded-3xl border-none bg-secondaryLogin w-fit drop-shadow-[0_0px_80px_rgba(255,255,255,0.70)]">
-      <img className="p-5 w-[209px] h-[139px]" src={image} alt="" />
-      <Button text={country} customStyle={" w-[220px] h-[50px] text-[18px] font-[500] tracking-widest cursor-default"} />
+      <div className="p-5 pb-2">
+        <img className=" w-[209px] h-[139px]" src={image} alt="" />
+        <p className="text-darkSecondary text-center pt-2 font-[500] text-[18px]">
+          {number}
+        </p>
+      </div>
+      <Button
+        text={country}
+        customStyle={
+          " w-[220px] h-[50px] text-[18px] font-[500] tracking-widest cursor-default"
+        }
+      />
     </div>
   );
 }
--- src/components/proxy-location/ProxyLocationComponent.jsx
+++ src/components/proxy-location/ProxyLocationComponent.jsx
@@ -1,46 +1,56 @@
-import React from 'react'
-import ProxyCountry from './ProxyCountry'
+import React from "react";
+import ProxyCountry from "./ProxyCountry";
 
 function ProxyLocationComponent() {
   return (
-    <div className=' text-darkSecondary px-[30px] sm:px-[100px] md:px-[130px] pt-[10px] pb-24 md:pb-10 lg:pb-24 font-urbanist'>
-        <h1 className=' text-center text-4xl font-bold my-14'>Top Residential Proxy locations</h1>
-        <div className='flex flex-wrap md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:mx-auto 2xl:w-[1200px] justify-center items-center'>
-          <ProxyCountry 
-            image={'/images/Flag-North-Korea.png'}
-            country={'North Korea'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Australia.png'}
-            country={'Australia'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Pakistan.png'}
-            country={'Pakistan'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Turkey.png'}
-            country={'Turkey'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Kenya.png'}
-            country={'Kenya'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-China.png'}
-            country={'China'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Kuwait.png'}
-            country={'Kuwait'}
-          />
-          <ProxyCountry 
-            image={'/images/Flag-Bahrain.png'}
-            country={'Bahrain'}
-          />
-        </div>
+    <div className=" text-darkSecondary px-[30px] sm:px-[100px] md:px-[130px] pt-[10px] pb-24 md:pb-10 lg:pb-24 font-urbanist">
+      <h1 className=" text-center text-4xl font-bold my-14">
+        Top Residential Proxy locations
+      </h1>
+      <div className="flex flex-wrap md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:mx-auto 2xl:w-[1200px] justify-center items-center">
+        <ProxyCountry
+          image={"/images/Flag-North-Korea.png"}
+          country={"North Korea"}
+          number={100000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Australia.png"}
+          country={"Australia"}
+          number={200000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Pakistan.png"}
+          country={"Pakistan"}
+          number={300000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Turkey.png"}
+          country={"Turkey"}
+          number={400000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Kenya.png"}
+          country={"Kenya"}
+          number={500000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-China.png"}
+          country={"China"}
+          number={600000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Kuwait.png"}
+          country={"Kuwait"}
+          number={700000}
+        />
+        <ProxyCountry
+          image={"/images/Flag-Bahrain.png"}
+          country={"Bahrain"}
+          number={800000}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProxyLocationComponent
+export default ProxyLocationComponent;
